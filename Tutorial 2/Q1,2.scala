c9e2c812fe2f3db72ce192154515baf73d766247
Harini Jayawardhana <109010571+harijayk@users.noreply.github.com>
2022-07-15 14:11:36 +0530

    Create Q1,2.scala

---
+++ Tutorial 2/Q1,2.scala
@@ -0,0 +1,20 @@
+object Question2 {
+    def main(args: Array[String]):Unit = {
+        var k:Int = 2;
+        var i:Int = 2;
+        var j:Int = 2;
+        var m:Int = 5;
+        var n:Int = 5;
+        var f:Float = 12.0f;
+        var g:Float = 4.0f;
+        var c:String = "X";
+
+        println(k+12*m);
+        println(m/j);
+        println(n%j);
+        println(m/j*j);
+        println(f+10*5+g);
+       // println(++i*n);
+
+    }
+} 
