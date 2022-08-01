d60bf7bf40087e814c4547dc8b0894a7477493c0
Harini Jayawardhana <109010571+harijayk@users.noreply.github.com>
2022-08-01 10:56:27 +0530

    Create Question1.scala

---
+++ Tutorial 4/Question1.scala
@@ -0,0 +1,9 @@
+object Q1 extends App{
+    def interest(amount: Double): Double = amount match{
+        case x if x<20000 => x*0.02
+        case x if x<200000 => x*0.04
+        case x if x<2000000 => x*0.035
+    }
+    
+    println(interest(10000))
+}
