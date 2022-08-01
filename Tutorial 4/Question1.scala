1a637712adfaf3a63d312acf3fcbdd45a09a37ba
Harini Jayawardhana <109010571+harijayk@users.noreply.github.com>
2022-08-01 20:39:06 +0530

    Update Question1.scala

--- Tutorial 4/Question1.scala
+++ Tutorial 4/Question1.scala
@@ -3,6 +3,7 @@ object Q1 extends App{
         case x if x<20000 => x*0.02
         case x if x<200000 => x*0.04
         case x if x<2000000 => x*0.035
+        case x if x>=2000000 => x*0.065
     }
     
     println(interest(10000))
