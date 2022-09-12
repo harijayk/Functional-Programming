d56eb6f51cf0ef39ec5650af1b53ee91efba350d
Harini Jayawardhana <109010571+harijayk@users.noreply.github.com>
2022-09-12 23:29:30 +0530

    Update Q2.scala

--- Tutorial 7/Q2.scala
+++ Tutorial 7/Q2.scala
@@ -1,20 +1,18 @@
-object Q2 extends App {
+class Rational(x:Int,y:Int){
+    require(y>0,"denominator can't be equal to zero")
+    private def gcd(x:Int,y:Int):Int=if(y==0) x else gcd(y,x%y)
+    def nume = x/gcd(x,y)
+    def denom = y/gcd(x,y)
+    def neg = new Rational(-this.nume,this.denom)
+
+    def +(r:Rational) = new Rational(this.nume*r.denom+r.nume*this.denom,this.denom*r.denom)
+
+    def -(r:Rational) = this+r.neg
+}
+object Q2 extends App{
     val x = new Rational(3,4)
     val y = new Rational(5,8)
     val z = new Rational(2,7)
-  
-    val a = x-y-z
-    
-    println(a)
-}
-
-class Rational(n:Int, d:Int) {
-    require(denom > 0, "Denominator must be greater than 0")
-    def numer = n
-    def denom = d
-    def neg = new Rational(-this.numer, this.denom)
-    def add(r:Rational) = new Rational(this.numer*r.denom + this.denom*r.numer, this.denom*r.denom)
-    def -(r:Rational) = this.add(r.neg)
-
-    override def toString(): String = numer + "/" + denom
+    val result = x-y-z
+    println("Value of " + x.nume + "/" + x.denom + "-" + y.nume + "/" + y.denom +"-"+z.nume +"/"+z.denom+"= "+result.nume +"/"+result.denom)
 }
